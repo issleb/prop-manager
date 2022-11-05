2c933d5385c3f9ce44aed88c14c43832e4c922a2
Brian Issleb <issleb@gmail.com>
2022-11-05 10:52:33 -0500

    Add home div to page.

--- airbnb.js
+++ airbnb.js
@@ -1,5 +1,15 @@
 import { SITES } from "./constants.js";
 
-const background = chrome.runtime.connect({ name: SITES.AIRBNB });
+const divName = "prop-manager";
 
+
+
+const background = chrome.runtime.connect({ name: SITES.AIRBNB });
 console.log(`Extension running on ${SITES.AIRBNB}.`);
+
+let homeDiv = document.getElementById(divName);
+if (!homeDiv)  {
+    homeDiv = document.createElement("div");
+    homeDiv.id = divName;
+    document.body.appendChild(homeDiv);              
+}
