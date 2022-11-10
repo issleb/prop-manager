7dcdb3d16ade695041a7dcd667e9d4126d011114
Brian Issleb <issleb@gmail.com>
2022-11-10 10:54:48 -0600

    Add metric tooltips.

--- src/app/Room/RoomCard.js
+++ src/app/Room/RoomCard.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 import "./RoomCard.scss";
 
 function RoomCard(props) {
     const room = props.room;
     console.log("props.room", props.room);
+
+
+    const renderOccupanyRate = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            We estimate your Occupany Rate is &nbsp;
+          <span style={{ color: room.metrics.occupancy.diff < 0 ? "red" : "green" }}>
+                                        {room.metrics.occupancy.diff}%
+                                    </span>
+
+                                    &nbsp; compared to equivalent properties.
+        </Tooltip>
+      );
+
+      const renderAdrRate = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            We estimate your Average Daily Rate is &nbsp;
+          <span style={{ color: room.metrics.adr.diff < 0 ? "red" : "green" }}>
+                                        {room.metrics.adr.diff}%
+                                    </span>
+
+                                    &nbsp; compared to equivalent properties.
+        </Tooltip>
+      );      
 
     return (
         <Card className="room-card">
@@ -16,23 +41,43 @@ function RoomCard(props) {
                 <Card.Subtitle className="address">{room.address}</Card.Subtitle>
 
                 <div className="room-card-subsection">
+
+                <OverlayTrigger
+                    placement="top"
+                    delay={{ show: 250, hide: 400 }}
+                    overlay={renderOccupanyRate}
+                    >
+                        <Card.Text>
+                            Occupancy: {room.metrics.occupancy.value}% 
+                                    <span style={{ color: room.metrics.occupancy.diff < 0 ? "red" : "green" }}>
+                                        ({room.metrics.occupancy.diff}%)
+                                    </span>
+                        </Card.Text>
+                    </OverlayTrigger>
+
+
                     <Card.Text>
-                        Occupancy: {room.metrics.occupancy.value}% 
-                                <span style={{ color: room.metrics.occupancy.diff < 0 ? "red" : "green" }}>
-                                    ({room.metrics.occupancy.diff}%)
-                                </span>
-                    </Card.Text>
-                    <Card.Text>
-                    ADR: {room.metrics.adr.value} 
+                    <OverlayTrigger
+                    placement="top"
+                    delay={{ show: 250, hide: 400 }}
+                    overlay={renderAdrRate}
+                    >
+                        <Card.Text>
+                        ADR: {room.metrics.adr.value} 
                             <span style={{ color: room.metrics.adr.diff < 0 ? "red" : "green" }}>
                                 ({room.metrics.adr.diff})
                             </span>
+                        </Card.Text>
+                    </OverlayTrigger>
+
+
                     </Card.Text>
                 </div>
 
 
                 <div className="room-card-subsection">
                     {!room.isAirBnb && (
+                        
                         <Card.Text>
                             <a href={room.airbnb.url} target="_blank">View on AirBnb</a>
                         </Card.Text>                        
